23f2cd4dc1f6266a44331e2bfdec3fd4ffa9a3ff
Matt-Mf <57960504+Matt-Mf@users.noreply.github.com>
2020-03-05 19:51:54 +0100

    up

---
+++ somma2numeri.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+ int numero1,numero2;
+ int res;
+ int somma(int numero1, int numero2);
+
+int main(void) {
+  printf("Programma Somma, Prego inserire addendi: \n");
+   scanf("%d", &numero1);
+   scanf("%d", &numero2);
+   res = somma(numero1, numero2);
+
+  printf("Somma = ");
+  printf("%d", res);
+
+  return (0);
+}
+
+// Funzione di somma
+int somma(int numero1, int numero2) {
+  res = numero1+numero2;
+  return (res);
+}
